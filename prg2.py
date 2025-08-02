14cc79ffca56f93655462c35b87439524cbc65e5
SanjanaMayya <mayyasanjana2004@gmail.com>
2025-08-02 10:25:04 +0530

    Add files via upload

---
+++ prg2.py
@@ -0,0 +1,13 @@
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route("/<int:number>")
+def even_func(number):
+    primes = ""
+    for i in range(1,number+1):
+        if(i % 2 != 0):
+            primes = primes + str(i) + ","
+    return primes
+
+if __name__ == "_main__":
+    app.run(debug=True)
---
+++ prg4.py
@@ -0,0 +1,17 @@
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route("/<int:number>")
+def primefunc(number):
+    primes = ""
+    for i in range(1,number+1):
+        for n in range(2,i):
+            if(i % n == 0):
+                break
+        else:
+            primes = primes + str(i) + ","
+    
+    return primes
+
+if __name__ == "_main__":
+    app.run(debug=True)
