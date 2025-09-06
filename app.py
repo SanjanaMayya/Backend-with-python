f0c310d8b8adb247702ee593c384246c943e8bcc
SanjanaMayya <mayyasanjana2004@gmail.com>
2025-09-06 10:32:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask 
-app = Flask(__name__)
+import mysql.connector 
+from flask import Flask ,Request, jsonify
 
-@app.route("/<int:number>")
-def prime(number):
-    primes = ""
-    for i in range(1,number+1):
-       primes = primes + str(i) + "," 
-    return primes
+app = Flask( __name__ )
 
-if __name__ == "__main__":
-    app.run()
+db= mysql.connector.connect (
+    host="localhost",
+    user="root",
+    password="",
+    database="peopledb"
+)
+cursor = db.cursor()
+ 
+@app.route('/users',methods=['GET'])
+def get_users():
+    cursor.execute("select * FROM users")
+    return jsonify (cursor.fetchall())
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+    
+@app.route('/createuser',methods=['post','GET'])
+def add_users():
+  if(Request.method !='post'):
+        return'''<form method="post">
+        username:<input type="text" name="name" required><br>
+        password:<input type="email" email="email" required><br>
+        <input type="submit"value="register">
+        </form>
+        '''
+  else:
+      name=Request.form["name"]      
+      email=Request.form["email"]
+      cursor.execute("INSERT INTO student")
